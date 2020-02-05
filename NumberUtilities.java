b8fe7d1c7a9737753af44973ca6e7e6e0dd949ed
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-05 15:52:36 -0500

    Exponents, how do they work?  Ha

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,86 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String output = "";
+
+        for(int i = 0; i < stop; i++) {
+            output += i;
+        }
+        return output;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String output = "";
+
+        for(int i = start; i < stop; i++)
+        {
+            output += i;
+        }
+
+        return output;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String output = "";
+
+        for(int i = start; i < stop; i += step)
+        {
+            output += i;
+        }
+
+        return output;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String output = "";
+
+        if((start % 2) != 0)
+        {
+            start++;
+        }
+
+        for(int i = start; i < stop; i += 2)
+        {
+            output += i;
+        }
+
+        return output;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String output = "";
+        
+        if((start % 2) == 0)
+        {
+            start++;
+        }
+
+        for(int i = start; i < stop; i += 2)
+        {
+            output += i;
+        }
+
+        return output;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String output = "";
+
+        for(int i = start; i <= stop; i++)
+        {
+            int x = i;
+
+            for(int n = 2; n <= exponent; n++)
+            {
+                x *= x;
+            }
+
+            output += x;
+        }
+
+        return output;
     }
 }
