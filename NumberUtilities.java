b844db73ef0be4f4a9f34df445b073fae6b61157
James Wilkinson <jwilkinson@zipcodes-MacBook-Pro.local>
2020-02-06 13:16:32 -0500

    Refactor

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -36,36 +36,22 @@ public class NumberUtilities {
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        String output = "";
-
         if((start % 2) != 0)
         {
             start++;
         }
 
-        for(int i = start; i < stop; i += 2)
-        {
-            output += i;
-        }
-
-        return output;
+        return getRange(start, stop, 2);
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        String output = "";
-        
         if((start % 2) == 0)
         {
             start++;
         }
 
-        for(int i = start; i < stop; i += 2)
-        {
-            output += i;
-        }
-
-        return output;
+        return getRange(start, stop, 2);
     }
 
 
